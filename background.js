3ad55b1fd56d6ec1087d06ecdacef694cc4b6f07
kalramanoj2002 <kalramanoj2002@gmail.com>
2017-01-22 23:55:17 -0500

    changes

--- background.js
+++ background.js
@@ -4,20 +4,15 @@ var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?socialtrade\.biz/;
 
 var current_tab;
 var _selectedTimings;
+var min = 32000;
+var max = 40000;
 
 // A function to use as callback
 function responseFromContentWindow(pendingElementsCount) {
 	console.log('Pending Elements:' + pendingElementsCount);
-	chrome.storage.sync.get({
-		'selectedTimings': 35000,
-	  }, function(items) {
-		_selectedTimings = items.selectedTimings;
-	  });
-	console.log('selectedTimings:'+ _selectedTimings);
-	if(_selectedTimings == undefined)
-	{
-		_selectedTimings = 35000;
-	}
+	var random = Math.floor(Math.random() * (max - min + 1)) + min;
+	_selectedTimings = random;
+	console.log("_selectedTimings: "+ _selectedTimings);
 	if(pendingElementsCount > 0){
 		setTimeout(clickLink, _selectedTimings);
 	}
@@ -29,6 +24,7 @@ function clickLink()
 	chrome.tabs.sendMessage(current_tab.id, {text: 'report_back'}, responseFromContentWindow);
 	}
 }
+
 
 // When the browser-action button is clicked...
 chrome.browserAction.onClicked.addListener(function (tab) {
